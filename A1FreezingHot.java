52f8f7f2925c92377ff19a8506dc94650f8ee6f5
Erick-Meza <erick.mezavilleda25@ycdsbk12.ca>
2024-03-27 18:33:55 -0400

    Completed A1

--- A1FreezingHot.java
+++ A1FreezingHot.java
@@ -1,1 +1,26 @@
+class A1FreezingHot extends ConsoleProgram{
+    /**
+     * A program that will prompt for two temperatures and determine if they meet the requirements
+     * @author: Erick M
+     */
 
+     public void run(){
+        
+        // Initializes variables
+        double dblTempOne;
+        double dblTempTwo;
+
+        // Prompts for user input
+        dblTempOne = readDouble("Please enter a temperature #1: ");
+        dblTempTwo = readDouble("Please enter temperature #2: ");
+
+        // Creates a conditional selective algorithm to determine if temperatures meet requirement
+        if ((dblTempOne < 0 || dblTempTwo < 0) || (dblTempOne > 120 || dblTempTwo > 120)){
+
+            // Outputs true
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+     }
+}
